e814578b0dac38950d93d0e81888157e20932c78
nicholasblackburn1 <nickblackburn02@gmail.com>
2020-07-06 10:56:52 -0400

    Got Ram useage to display in mc

---
+++ src/main/java/space/nicholasblackburn/waterspigot/Waterspigot.java
@@ -0,0 +1,35 @@
+package space.nicholasblackburn.waterspigot;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Waterspigot extends JavaPlugin implements Listener {
+
+    
+    // Runs on Plugin ENabled / Server Starting 
+    @Override
+    public void onEnable() {
+        
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            /*
+             * We register the EventListeneres here, when PlaceholderAPI is installed.
+             * Since all events are in the main class (this class), we simply use "this"
+             */
+
+             
+            Bukkit.getPluginManager().registerEvents(this, this);
+        } else {
+            throw new RuntimeException("Could not find PlaceholderAPI!! Plugin can not work without it!");
+        }
+    
+    }
+
+    // Runs on Server Disabled is Called
+    @Override
+    public void onDisable(){
+
+    }
+
+
+}
